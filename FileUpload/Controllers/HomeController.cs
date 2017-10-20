2cd7fbdeb6a4b64a1253bdb272cf0dd4072d1054
dhani.057 <dhani.057@gmail.com>
2017-10-20 10:54:11 +0700

    Add project files.

---
+++ FileUpload/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.OleDb;
+using System.IO;
+using FileUpload.Models;
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+        OleDbConnection Econ;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult View()
+        {
+            dbFilesEntities entities = new dbFilesEntities();
+            return View(from tbl_registration in entities.tbl_registration.Take(10)
+                        select tbl_registration);
+        }
+
+        [HttpPost]
+        public ActionResult View(HttpPostedFileBase file)
+        {
+            string filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            string filepath = "/excelfolder/" + filename;
+            file.SaveAs(Path.Combine(Server.MapPath("/excelfolder"), filename));
+            InsertExceldata(filepath, filename);
+
+            return View();
+        }
+
+        private void ExcelConn(string filepath)
+        {
+            string constr = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=""Excel 12.0 Xml;HDR=YES;""", filepath);
+            Econ = new OleDbConnection(constr);
+
+        }
+
+        private void InsertExceldata(string fileepath, string filename)
+        {
+            string fullpath = Server.MapPath("/excelfolder/") + filename;
+            ExcelConn(fullpath);
+            string query = string.Format("Select * from [{0}]", "Sheet1$");
+            OleDbCommand Ecom = new OleDbCommand(query, Econ);
+            Econ.Open();
+
+            DataSet ds = new DataSet();
+            OleDbDataAdapter oda = new OleDbDataAdapter(query, Econ);
+            Econ.Close();
+            oda.Fill(ds);
+
+            DataTable dt = ds.Tables[0];
+
+            SqlBulkCopy objbulk = new SqlBulkCopy(con);
+            objbulk.DestinationTableName = "tbl_registration";
+            objbulk.ColumnMappings.Add("Email", "Email");
+            objbulk.ColumnMappings.Add("Password", "Password");
+            objbulk.ColumnMappings.Add("Name", "Name");
+            objbulk.ColumnMappings.Add("Address", "Address");
+            objbulk.ColumnMappings.Add("City", "City");
+            objbulk.ColumnMappings.Add("Age", "Age");
+            con.Open();
+            objbulk.WriteToServer(dt);
+            con.Close();
+        }
+    }
+}
