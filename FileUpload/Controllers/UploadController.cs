c721e6c3ce2d5e57390d440c2be11e4f5ba7e584
dhani.057 <dhani.057@gmail.com>
2017-10-26 20:23:08 +0700

    + Operation

---
+++ FileUpload/Controllers/UploadController.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.OleDb;
+using System.IO;
+using FileUpload.Models;
+using System.Linq.Dynamic;
+using System.Net;
+using System.Data.Entity;
+
+namespace FileUpload.Controllers
+{
+    public class UploadController : Controller
+    {
+        private dbFilesEntities db = new dbFilesEntities();
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+        OleDbConnection Econ;
+
+        // GET: Upload
+        public ActionResult Index(int page = 1, string sort = "Name", string sortdir = "asc", string search = "")
+        {
+            int pageSize = 10;
+            int totalRecord = 0;
+            if (page < 1) page = 1;
+            int skip = (page * pageSize) - pageSize;
+            var data = GetData(search, sort, sortdir, skip, pageSize, out totalRecord);
+            ViewBag.TotalRows = totalRecord;
+            ViewBag.search = search;
+            return View(data);
+        }
+
+        // GET: Upload
+        public ActionResult View(int page = 1, string sort = "Name", string sortdir = "asc", string search = "")
+        {
+            int pageSize = 10;
+            int totalRecord = 0;
+            if (page < 1) page = 1;
+            int skip = (page * pageSize) - pageSize;
+            var data = GetData(search, sort, sortdir, skip, pageSize, out totalRecord);
+            ViewBag.TotalRows = totalRecord;
+            ViewBag.search = search;
+            return View(data);
+        }
+
+        public List<tbl_registration> GetData(string search, string sort, string sortdir, int skip, int pageSize, out int totalRecord)
+        {
+            using (dbFilesEntities dc = new dbFilesEntities())
+            {
+                var v = (from a in dc.tbl_registration
+                         where
+                                 a.Email.Contains(search) ||
+                                 a.Name.Contains(search) ||
+                                 a.City.Contains(search)
+                         select a
+                                );
+                totalRecord = v.Count();
+                v = v.OrderBy(sort + " " + sortdir);
+                if (pageSize > 0)
+                {
+                    v = v.Skip(skip).Take(pageSize);
+                }
+                return v.ToList();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            string filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            string filepath = "/excelfolder/" + filename;
+            file.SaveAs(Path.Combine(Server.MapPath("/excelfolder"), filename));
+            InsertExceldata(filepath, filename);
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult View(HttpPostedFileBase file)
+        {
+            string filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            string filepath = "/excelfolder/" + filename;
+            file.SaveAs(Path.Combine(Server.MapPath("/excelfolder"), filename));
+            InsertExceldata(filepath, filename);
+
+            return View();
+        }
+
+        private void ExcelConn(string filepath)
+        {
+            string constr = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=""Excel 12.0 Xml;HDR=YES;""", filepath);
+            Econ = new OleDbConnection(constr);
+
+        }
+
+        private void InsertExceldata(string fileepath, string filename)
+        {
+            string fullpath = Server.MapPath("/excelfolder/") + filename;
+            ExcelConn(fullpath);
+            string query = string.Format("Select * from [{0}]", "Sheet1$");
+            OleDbCommand Ecom = new OleDbCommand(query, Econ);
+            Econ.Open();
+
+            DataSet ds = new DataSet();
+            OleDbDataAdapter oda = new OleDbDataAdapter(query, Econ);
+            Econ.Close();
+            oda.Fill(ds);
+
+            DataTable dt = ds.Tables[0];
+
+            SqlBulkCopy objbulk = new SqlBulkCopy(con);
+            objbulk.DestinationTableName = "tbl_registration";
+            objbulk.ColumnMappings.Add("Email", "Email");
+            objbulk.ColumnMappings.Add("Password", "Password");
+            objbulk.ColumnMappings.Add("Name", "Name");
+            objbulk.ColumnMappings.Add("Address", "Address");
+            objbulk.ColumnMappings.Add("City", "City");
+            objbulk.ColumnMappings.Add("Age", "Age");
+            con.Open();
+            objbulk.WriteToServer(dt);
+            con.Close();
+        }
+
+        // GET: Upload/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_registration tbl_registration = db.tbl_registration.Find(id);
+            if (tbl_registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_registration);
+        }
+
+        // GET: Upload/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Upload/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Sr_no,Email,Password,Name,Address,City,Age,Status")] tbl_registration tbl_registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_registration.Add(tbl_registration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbl_registration);
+        }
+
+        // GET: Upload/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_registration tbl_registration = db.tbl_registration.Find(id);
+            if (tbl_registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_registration);
+        }
+
+        // POST: Upload/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Sr_no,Email,Password,Name,Address,City,Age,Status")] tbl_registration tbl_registration)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_registration).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbl_registration);
+        }
+
+        // GET: Upload/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_registration tbl_registration = db.tbl_registration.Find(id);
+            if (tbl_registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_registration);
+        }
+
+        // POST: Upload/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_registration tbl_registration = db.tbl_registration.Find(id);
+            db.tbl_registration.Remove(tbl_registration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
